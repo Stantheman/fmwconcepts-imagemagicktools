8f5c7fa58821577bf62d18ff039dfe1f63b21053
sschwertly <sschwertly@etsy.com>
2017-12-22 23:19:52 -0500

    fix awk to not break on varying amounts of dashes

--- fmwconcepts-update.sh
+++ fmwconcepts-update.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 cd ./bin/
 wget 'http://www.fmwconcepts.com/imagemagick/script_list.txt' -O script_list.txt
-awk -F\-\- '{ print $1}' script_list.txt | egrep '^\w' | while read name; do
+awk -F\- '{ print $1}' script_list.txt | egrep '^\w' | while read name; do
     wget "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=${name}&dirname=${name}" -O ${name}
     chmod +x ${name}
 done
